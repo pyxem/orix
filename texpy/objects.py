17b955b09ead59473891bcff7f77c453335c3ebe
bm424 <bm424@cam.ac.uk>
2018-10-19 19:27:06 +0100

    Add API options for loading typical objects

---
+++ texpy/objects.py
@@ -0,0 +1,2 @@
+from texpy.quaternion.orientation import Orientation, Misorientation
+from texpy.vector.neo_euler import *
---
+++ texpy/symmetry.py
@@ -0,0 +1,2 @@
+from texpy.quaternion.symmetry import *
+from texpy.quaternion.orientation_region import OrientationRegion
