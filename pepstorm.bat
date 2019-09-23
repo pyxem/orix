e56463c9d5d9d038efffcae09dfe5ad4637d5784
Duncan Johnstone <duncanjohnstone@live.co.uk>
2019-09-23 09:37:12 +0100

    update pepstorm.bat

--- pepstorm.bat
+++ pepstorm.bat
@@ -1,14 +1,7 @@
 @echo off
 set root_dir=%~dp0
 pushd %root_dir%
-cd pyxem/tests
-for /D %%G in ("test_*") do (
-    cd %%G
-	for /R %%G in ("*.py") do autopep8 --aggressive --in-place --max-line-length 130 %%G
-	cd ..
-)
-cd ../
-for %%G in (components generators libraries signals utils) do (
+for %%G in (base grid io plot quaternion scalar tests vector) do (
 	cd %%G
 	for /R %%G in ("*.py") do autopep8 --aggressive --in-place --max-line-length 130 %%G
 	cd ..
