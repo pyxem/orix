94205ec92ff65b92b3e24e15fb1006a2e9c4ba6c
pc494 <pc494@cam.ac.uk>
2019-09-24 16:00:53 +0100

    license + other packaging updates

--- setup.py
+++ setup.py
@@ -5,14 +5,31 @@ from orix import __name__, __version__, __author__, __author_email__, __descript
 setup(
     name=__name__,
     version=str(__version__),
-    license="MIT",
+    license="GPLv3",
     author=__author__,
     author_email=__author_email__,
     description=__description__,
+    long_description=open('README.rst').read(),
+    classifiers=[
+	"Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+	"Programming Language :: Python :: 3.7",
+	"Development Status :: 4 - Beta",
+	"Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
     packages=find_packages(exclude=['orix/tests']),
     install_requires=[
         "numpy",
         "scipy",
         "matplotlib",
-    ]
+    ],
+    package_data={
+        "": ["LICENSE", "readme.rst"],
+        "diffsims": ["*.py"],
+    },
 )
