016f1b6a1fb45efa4097a602a49d152335081d08
Duncan N. Johnstone <duncanjohnstone@live.co.uk>
2019-09-24 16:40:35 +0100

    Update __init__.py

--- orix/__init__.py
+++ orix/__init__.py
@@ -13,7 +13,7 @@ Submodules
 """
 
 __name__ = "orix"
-__version__ = "0.1.1dev"
+__version__ = "0.1.1"
 __author__ = "Ben Martineau"
 __author_email__ = "pyxem.team@gmail.com"
 __description__ = "Orientation, rotation, quaternion, and crystal symmetry handling in Python."
