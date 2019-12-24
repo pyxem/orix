375464d3ba98123ef2753486e0aa8b891b6f5ab2
pc494 <pc494@cam.ac.uk>
2019-12-24 22:37:24 +0000

    Fix syntax error discovered via testing

--- orix/quaternion/orientation.py
+++ orix/quaternion/orientation.py
@@ -192,7 +192,7 @@ class Orientation(Misorientation):
     def __sub__(self, other):
         if isinstance(other, Orientation):
             misorientation = Misorientation(self * ~other)
-            m_inside = misorientation.set_symmetry((self.symmetry, other.symmetry)).squeeze()
+            m_inside = misorientation.set_symmetry(self.symmetry, other.symmetry).squeeze()
             return m_inside
         return NotImplemented
 
