95126c593292dd232bc6a5b6b888b2f4d01b69d6
pc494 <pc494@cam.ac.uk>
2019-12-27 21:10:21 +0000

    Test that .angle throws for homochoric

--- orix/tests/test_neoeuler.py
+++ orix/tests/test_neoeuler.py
@@ -5,6 +5,7 @@ from orix.vector.neo_euler import Rodrigues,Homochoric
 from orix.quaternion.rotation import Rotation
 
 
+""" Rodrigues """
 @pytest.mark.parametrize('rotation, expected', [
     (Rotation([1, 0, 0, 0]), [0, 0, 0]),
     (Rotation([0.9239, 0.2209, 0.2209, 0.2209]), [0.2391, 0.2391, 0.2391]),
@@ -21,9 +22,18 @@ def test_angle(rodrigues, expected):
     angle = rodrigues.angle
     assert np.allclose(angle.data, expected, atol=1e-3)
 
+""" Homochoric"""
 @pytest.mark.parametrize('rotation', [
     Rotation([1, 0, 0, 0]),
     Rotation([0.9239, 0.2209, 0.2209, 0.2209])])
 def test_Homochoric_from_rotation(rotation):
     h = Homochoric.from_rotation(rotation)
     return None
+
+@pytest.mark.parametrize('rotation', [
+    Rotation([1, 0, 0, 0]),
+    Rotation([0.9239, 0.2209, 0.2209, 0.2209])])
+@pytest.mark.xfail(strict=True,reason=AttributeError)
+def test_Homochoric_angle(rotation):
+    h = Homochoric.from_rotation(rotation)
+    h.angle
