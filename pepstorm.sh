beebb4047ac012dc13ed855d9f4f24746bf7fefd
pc494 <pc494@cam.ac.uk>
2020-01-06 17:37:07 +0000

    More pepstorm adjustments

--- pepstorm.sh
+++ pepstorm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 cd orix/
-for folder in base grid io plot quaternion scalar tests vector
+for folder in base io plot quaternion scalar tests vector
 	do
 	cd $folder
 	autopep8 *.py --aggressive --in-place --max-line-length 130
