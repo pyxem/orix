2c4221677ad650322a7a458ded9039be7b7585c7
Duncan Johnstone <duncanjohnstone@live.co.uk>
2020-07-20 22:16:05 +0100

    add python 3.8 to description

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
