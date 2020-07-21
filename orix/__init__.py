0432fb67ca55d8619ee9928023f8fde5edfe033a
Duncan N. Johnstone <duncanjohnstone@live.co.uk>
2020-07-21 16:03:17 +0100

    Bump version to 0.3.1dev

--- orix/__init__.py
+++ orix/__init__.py
@@ -1,5 +1,5 @@
 __name__ = "orix"
-__version__ = "0.3.0"
+__version__ = "0.3.1dev"
 __author__ = "Ben Martineau, Phillip Crout, Håkon Wiik Ånes, Duncan Johnstone"
 __author_email__ = "pyxem.team@gmail.com"
 __description__ = (
