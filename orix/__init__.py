b55ce8c335b9198db15b8afe8f362d224c5693a1
Duncan N. Johnstone <duncanjohnstone@live.co.uk>
2020-07-21 16:02:54 +0100

    Update __description__

--- orix/__init__.py
+++ orix/__init__.py
@@ -3,7 +3,7 @@ __version__ = "0.3.0"
 __author__ = "Ben Martineau, Phillip Crout, Håkon Wiik Ånes, Duncan Johnstone"
 __author_email__ = "pyxem.team@gmail.com"
 __description__ = (
-    "Orientation, rotation, quaternion, and crystal symmetry handling in Python."
+    "orix is an open-source python library for handling crystal orientation mapping data."
 )
 __credits__ = [
     "Ben Martineau",
