db7f971d7d0f58f73f496029440d730011df73fe
phillip <pc494@cam.ac.uk>
2020-07-26 21:20:35 +0100

    port from diffsims, fundamental_zone_generators

--- orix/gridding/gridding_utils.py
+++ orix/gridding/gridding_utils.py
@@ -18,3 +18,57 @@
 
 """ This file contains functions (broadly internal ones) that support
 the grid generation within rotation space """
+
+def get_proper_point_group_string(space_group_number):
+    """
+    Maps a space-group-number to a point group
+
+    Parameters
+    ----------
+    space_group_number : int
+
+    Returns
+    -------
+    point_group_str : str
+        The proper point group string in --- convention
+
+    Notes
+    -----
+    This function enumerates the list on https://en.wikipedia.org/wiki/List_of_space_groups
+    Point groups (32) are converted to proper point groups (11) using the Schoenflies
+    representations given in that table.
+    """
+
+    if space_group_number in [1, 2]:
+        return "1"  # triclinic
+    if 2 < space_group_number < 16:
+        return "2"  # monoclinic
+    if 15 < space_group_number < 75:
+        return "222"  # orthorhomic
+    if 74 < space_group_number < 143:  # tetragonal
+        if (74 < space_group_number < 89) or (99 < space_group_number < 110):
+            return "4"  # cyclic
+        else:
+            return "422"  # dihedral
+    if 142 < space_group_number < 168:  # trigonal
+        if 142 < space_group_number < 148 or 156 < space_group_number < 161:
+            return "3"  # cyclic
+        else:
+            return "32"  # dihedral
+    if 167 < space_group_number < 194:  # hexagonal
+        if 167 < space_group_number < 176 or space_group_number in [183, 184, 185, 186]:
+            return "6"  # cyclic
+        else:
+            return "622"  # dihedral
+    if 193 < space_group_number < 231:  # cubic
+        if 193 < space_group_number < 207 or space_group_number in [
+            215,
+            216,
+            217,
+            218,
+            219,
+            220,
+        ]:
+            return "432"  # oct
+        else:
+            return "23"  # tet
