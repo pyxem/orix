12daa3cc02e5a94a08d7ccabb8e5d765ecb26895
phillip <pc494@cam.ac.uk>
2020-07-29 11:22:01 +0100

    lower bound on matplotlib

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "diffpy.structure >= 3",
         "h5py",
-        "matplotlib",
+        "matplotlib > 3.2",
         "numpy",
         "scipy",
         "tqdm",
