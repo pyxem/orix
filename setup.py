7e5a7010f98b40cbd5e22356d060dba997ccd5cf
phillip <pc494@cam.ac.uk>
2020-08-01 16:24:53 +0100

    improve versioning clarity

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "diffpy.structure >= 3",
         "h5py",
-        "matplotlib > 3.2",
+        "matplotlib >= 3.3",
         "numpy",
         "scipy",
         "tqdm",
