aad2b2d50242ce78ed6afc534720ab03745d0a1d
Phillip Crout <32455330+pc494@users.noreply.github.com>
2020-08-20 13:38:37 +0100

    version to 0.4.0-rc1

--- orix/__init__.py
+++ orix/__init__.py
@@ -1,5 +1,5 @@
 __name__ = "orix"
-__version__ = "0.4.0dev"
+__version__ = "0.4.0-rc1"
 __author__ = "Ben Martineau, Phillip Crout, Håkon Wiik Ånes, Duncan Johnstone"
 __author_email__ = "pyxem.team@gmail.com"
 __description__ = "orix is an open-source python library for handling crystal orientation mapping data."
