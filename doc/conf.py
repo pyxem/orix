d5db4020413f018f190371f04cd134780afbac45
Håkon Wiik Ånes <hwaanes@gmail.com>
2020-08-21 11:08:33 +0200

    Remove unnecessary path addition in readthedocs config file
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ from orix import __author__, __version__, __file__
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath("."))
+# sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Release info ------------------------------------------------------------
