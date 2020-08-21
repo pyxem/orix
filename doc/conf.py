adbdd2a5778e5f54d18578858a5901cc6d6b422a
Håkon Wiik Ånes <hwaanes@gmail.com>
2020-08-21 11:01:23 +0200

    Move readthedocs config file to correct location
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -13,14 +13,13 @@ from os.path import relpath, dirname
 import re
 import sys
 
+from orix import __author__, __version__, __file__
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# sys.path.insert(0, os.path.abspath("."))
-sys.path.append("../")
+sys.path.insert(0, os.path.abspath("."))
 
-from orix import __author__, __version__, __file__
 
 # -- Release info ------------------------------------------------------------
 
