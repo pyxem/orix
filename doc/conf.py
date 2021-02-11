0e41927cd93716c6aae57ceef8b1817a8be62f67
Håkon Wiik Ånes <hwaanes@gmail.com>
2021-02-11 22:51:54 +0100

    Update documentation to use Jupyter notebooks, add contrib. guide
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -4,8 +4,6 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
 from datetime import datetime
 import inspect
 import os
@@ -21,35 +19,35 @@ from orix import __author__, __version__, __file__
 # sys.path.insert(0, os.path.abspath("."))
 
 
-# -- Release info ------------------------------------------------------------
-
 project = "orix"
 copyright = f"2018-{str(datetime.now().year)}, {__author__}"
 author = __author__
 release = __version__
 
 
-# -- General configuration ---------------------------------------------------
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    #    "sphinxcontrib.bibtex",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx.ext.linkcode",
+    "sphinx_gallery.load_style",
+    "nbsphinx",
 ]
 
 # Create links to references within orix's documentation to these packages
 intersphinx_mapping = {
+    "diffpy.structure": ("https://www.diffpy.org/diffpy.structure", None),
+    "h5py": ("http://docs.h5py.org/en/stable/", None),
     "matplotlib": ("https://matplotlib.org", None),
     "numpy": ("https://docs.scipy.org/doc/numpy", None),
     "python": ("https://docs.python.org/3", None),
     "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
-    "h5py": ("http://docs.h5py.org/en/stable/", None),
 }
 
 # Add any paths that contain templates here, relative to this directory.
@@ -60,9 +58,6 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
-# -- Options for HTML output -------------------------------------------------
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = "sphinx_rtd_theme"
@@ -72,9 +67,66 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# Syntax highlighting
+pygments_style = "friendly"
+
 # Logo
 html_logo = "_static/img/orix_logo.png"
 html_favicon = "_static/img/orix_logo.png"
+
+# nbsphinx configuration
+# Taken from nbsphinx' own nbsphinx configuration file, with slight
+# modification to point nbviewer and Binder to the GitHub master links
+# when the documentation is launched from a orix version with "dev" in
+# the version
+if "dev" in __version__:
+    release_version = "master"
+else:
+    release_version = "v" + __version__
+# This is processed by Jinja2 and inserted before each notebook
+nbsphinx_prolog = (
+    r"""
+{% set docname = 'doc/' + env.doc2path(env.docname, base=None) %}
+
+.. raw:: html
+
+    <style>a:hover { text-decoration: underline; }</style>
+
+    <div class="admonition note">
+      This page was generated from
+      <a class="reference external" href="https://github.com/pyxem/orix/blob/"""
+    + f"{release_version}"
+    + r"""/{{ docname|e }}">{{ docname|e }}</a>.
+      Interactive online version:
+      <span style="white-space: nowrap;"><a href="https://mybinder.org/v2/gh/pyxem/orix/"""
+    + f"{release_version}"
+    + r"""?filepath={{ docname|e }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>.</span>
+      <script>
+        if (document.location.host) {
+          $(document.currentScript).replaceWith(
+            '<a class="reference external" ' +
+            'href="https://nbviewer.jupyter.org/url' +
+            (window.location.protocol == 'https:' ? 's/' : '/') +
+            window.location.host +
+            window.location.pathname.slice(0, -4) +
+            'ipynb">View in <em>nbviewer</em></a>.'
+          );
+        }
+      </script>
+    </div>
+
+.. raw:: latex
+
+    \nbsphinxstartnotebook{\scriptsize\noindent\strut
+    \textcolor{gray}{The following section was generated from
+    \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
+"""
+)
+# https://nbsphinx.readthedocs.io/en/0.8.0/never-execute.html
+nbsphinx_execute = "always"  # auto, always, never
+
+# sphinxcontrib-bibtex configuration
+# bibtex_bibfiles = ["bibliography.bib"]
 
 
 def linkcode_resolve(domain, info):
---
+++ doc/related_projects.rst
@@ -0,0 +1,14 @@
+================
+Related projects
+================
+
+This is a non-exhaustive list of related, open-source projects that users of orix might
+find useful:
+
+- `pyxem <https://github.com/pyxem/pyxem>`_: Python library for multi-dimensional
+  diffraction microscopy.
+- `diffsims <https://diffsims.rtfd.io>`_: Python library for simulating diffraction.
+- `kikuchipy <https://kikuchipy.org>`_: Python library for processing and analysis of
+  electron backscatter diffraction (EBSD) patterns.
+- `MTEX <https://mtex-toolbox.github.io>`_: Matlab toolbox for analyzing and
+  modelling crystallographic textures by means of EBSD or pole figure data.
