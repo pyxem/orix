74bf060652b2367437b4cd0de27c441092e417ff
Paddy Harrison <pj.harrison31@gmail.com>
2021-11-10 17:20:27 +0100

    remove smallest angle assignment from test

--- orix/tests/test_miller.py
+++ orix/tests/test_miller.py
@@ -222,7 +222,6 @@ class TestMiller:
     def test_multiply_orientation(self):
         o = Orientation.from_euler(np.deg2rad([45, 0, 0]))
         o.symmetry = CUBIC_PHASE.point_group
-        o = o.compute_symmetry_reduced_orientations()
         m = Miller(hkl=[[1, 1, 1], [2, 0, 0]], phase=CUBIC_PHASE)
         m2 = o * m
         assert isinstance(m2, Miller)
