7055c03eb28fc4e9ce92890519373716f21d0897
Paddy Harrison <pj.harrison31@gmail.com>
2021-12-11 19:34:13 +0100

    move Arrow3D to plot._util

--- orix/plot/unit_cell_plot.py
+++ orix/plot/unit_cell_plot.py
@@ -23,34 +23,11 @@ orientation.
 from itertools import combinations, product
 
 from diffpy.structure import Structure, Lattice
-from matplotlib.patches import FancyArrowPatch
-from mpl_toolkits.mplot3d import proj3d
 import matplotlib.pyplot as plt
 import numpy as np
 
 from orix.vector import Vector3d
-
-# taken from SO post https://stackoverflow.com/a/22867877/12063126
-class Arrow3D(FancyArrowPatch):
-    "Matplotlib arrows in 3D."
-
-    def __init__(self, xs, ys, zs, *args, **kwargs):
-        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
-        self._verts3d = xs, ys, zs
-
-    def draw(self, renderer):
-        xs3d, ys3d, zs3d = self._verts3d
-        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
-        # no in plane projection, ie. vector is vertical
-        # if np.allclose(np.flatten((xs, ys)), 0):
-        #     self.axes.plot3D(0, 0, 0, m="o")
-        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
-        FancyArrowPatch.draw(self, renderer)
-        zorder = 1
-        return zorder
-
-    # matplotlib>=3.5 compatibility
-    do_3d_projection = draw
+from orix.plot._util import Arrow3D
 
 
 def _calculate_basic_unit_cell_vertices(a1, a2, a3):
