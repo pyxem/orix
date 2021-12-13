925750cad99a4902bb81ad3783343c3059b4e4ee
Paddy Harrison <pj.harrison31@gmail.com>
2021-12-13 22:31:45 +0100

    tidy up

--- orix/plot/unit_cell_plot.py
+++ orix/plot/unit_cell_plot.py
@@ -34,7 +34,7 @@ def _calculate_basic_unit_cell_vertices(vectors):
     """Calculate cell vertices for unit cells."""
     vectors = np.asarray(vectors)
     assert vectors.shape == (3, 3), "Vectors must be (3, 3) array."
-    # generate list of lattice basis vectors from (000) to (111) (positive only)
+    # generate list of lattice basis vectors from (000) to (111) (+ve)
     verts = np.array(list(product(*zip((0, 0, 0), (1, 1, 1)))))
     verts = (verts[..., np.newaxis] * vectors).sum(axis=1)
     center = verts.mean(axis=0)
@@ -48,13 +48,15 @@ def _calculate_basic_unit_cell_edges(verts, vectors):
     a1, a2, a3 = np.linalg.norm(vectors, axis=-1)
     # get valid edges from all unit cell egde possibilities unit cell
     edges_valid = []
-    # for all possible combinations of vertices, keep if the distance between them is
-    # equal to any of the basis vectors
+    # for all possible combinations of vertices, keep if the distance
+    # between them is equal to any of the basis vectors
     for v1, v2 in combinations(verts, 2):
         if np.isclose((a1, a2, a3), np.linalg.norm(v2 - v1)).any():
-            # extra case for hexagonal unit cells, do not plot (0000)-(11-20) edge
-            # in hexagonal cells the gamma angle is 120 degrees (between a1 and a2)
-            # if xy of (v2 - v1) is equal to xy of vectors[0] + vectors[1] -> skip
+            # extra case for hexagonal unit cells, do not plot
+            # (0000)-(11-20) edge
+            # in hexagonal cells the gamma angle is 120 degrees
+            # (between a1 and a2). If xy of (v2 - v1) is equal to xy of
+            # vectors[0] + vectors[1] -> skip
             if np.isclose((v2 - v1)[:-1], (vectors[0] + vectors[1])[:-1]).all():
                 continue
             edges_valid.append((v1, v2))
@@ -69,7 +71,8 @@ def _plot_unit_cell(
     crystal_axes_loc="origin",
     **arrow_kwargs,
 ):
-    """Plot the unit cell orientation, showing the sample and crystal reference frames.
+    """Plot the unit cell orientation, showing the sample and crystal
+    reference frames.
 
     Parameters
     ----------
@@ -80,26 +83,27 @@ def _plot_unit_cell(
     axes_length : float, optional
         Length of the reference axes in Angstroms, by default 0.5.
     structure : diffpy.structure.Structure or None, optional
-        Structure of the unit cell, only orthorhombic lattices are currently
-        supported. If not given, a cubic unit cell with a lattice parameter of
-        2 Angstroms will be plotted.
+        Structure of the unit cell, only orthorhombic lattices are
+        currently supported. If not given, a cubic unit cell with a
+        lattice parameter of 2 Angstroms will be plotted.
     crystal_axes_loc : str, optional
-        Plot the crystal reference frame axes at the "origin" (default) or
-        "center" of the plotted cell.
+        Plot the crystal reference frame axes at the "origin" (default)
+        or "center" of the plotted cell.
     arrow_kwargs : dict, optional
         Keyword arguments passed to
-        :class:`matplotlib.patches.FancyArrowPatch`, for example "arrowstyle".
+        :class:`matplotlib.patches.FancyArrowPatch`, for example
+        "arrowstyle".
 
     Returns
     -------
     fig : matplotlib.figure.Figure
         The plotted figure.
     """
-    # requires active rotation of the lattice in the sample reference frame
+    # active rotation of the lattice in the sample reference frame
     inv_rotation = ~rotation
 
     crystal_axes_loc = crystal_axes_loc.lower()
-    if not crystal_axes_loc in ("origin", "center"):
+    if crystal_axes_loc not in ("origin", "center"):
         raise ValueError('Crystal_axes_loc must be either "origin" or "center".')
     # TODO: More than only cubic
     # introduce some basic non-cubic cell functionality
@@ -130,9 +134,11 @@ def _plot_unit_cell(
     ax.set_box_aspect((1, 1, 1))  # equal aspect
     ax.margins(0, 0, 0)
 
-    # default projection to +x -> east/right, +y -> north/upwards, +z out-of-page
+    # default projection to +x -> east/right, +y -> north/upwards,
+    # +z out-of-page
     ax.azim = -90
-    ax.elev = 90 - 1e-6  # add small offset to avoid arrow3D projection errors
+    # add small offset to avoid arrow3D projection errors
+    ax.elev = 90 - 1e-6
 
     # axes colors from ParaView and TomViz
     colors = ("tab:red", "yellow", "tab:green")
@@ -167,16 +173,14 @@ def _plot_unit_cell(
     if crystal_axes_loc == "origin":  # cell origin
         crys_ref_ax_origin = Vector3d(verts[0])
     else:
-        crys_ref_ax_origin = Vector3d(np.zeros(3))
+        crys_ref_ax_origin = Vector3d.zero((1,))
+    # rotate cell origin into new position
+    cell_origin_rotated = inv_rotation * crys_ref_ax_origin
 
     for i, v in enumerate(v_ref_ax):
-        # rotate vector
-        v1 = v.data.ravel() * axes_length
         # setup verts for reference axes
         data = np.zeros((3, 2))
-        data[:, 1] += v1
-        # rotate cell origin into new position
-        cell_origin_rotated = inv_rotation * crys_ref_ax_origin
+        data[:, 1] = v.data.ravel() * axes_length
         # offset axes to sit on cell origin
         data = (data.T + cell_origin_rotated.data).T
         label = labels[i]
