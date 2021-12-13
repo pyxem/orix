188ea0999b8d3fff43b30765b495ecca1e1dbfaa
Paddy Harrison <pj.harrison31@gmail.com>
2021-12-13 22:44:39 +0100

    reinstate renderer

--- orix/plot/_util.py
+++ orix/plot/_util.py
@@ -31,11 +31,11 @@ class Arrow3D(FancyArrowPatch):
         FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
         self._verts3d = xs, ys, zs
 
-    def draw(self, *args, **kwargs):
+    def draw(self, renderer):
         xs3d, ys3d, zs3d = self._verts3d
         xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
         self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
-        FancyArrowPatch.draw(self, *args, **kwargs)
+        FancyArrowPatch.draw(self, renderer)
         zorder = 1
         return zorder
 
