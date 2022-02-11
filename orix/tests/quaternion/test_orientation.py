d6df224eb62a8ac40213cbfdff86e7ea384f42ae
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-02-11 21:07:16 +0100

    Remove unnecessary import
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- orix/tests/quaternion/test_orientation.py
+++ orix/tests/quaternion/test_orientation.py
@@ -15,7 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with orix.  If not, see <http://www.gnu.org/licenses/>.
-import sysconfig
 
 import matplotlib.pyplot as plt
 import numpy as np
