8100268dbf236fdc6d4f02e1756cfbb5a6c0c22d
Paddy Harrison <pj.harrison31@gmail.com>
2022-02-25 15:25:43 +0100

    remove v3d check

--- orix/plot/crystal_map_plot.py
+++ orix/plot/crystal_map_plot.py
@@ -183,8 +183,6 @@ class CrystalMapPlot(Axes):
             for _, p in crystal_map.phases_in_data:
                 patches.append(mpatches.Patch(color=p.color_rgb, label=p.name))
         else:  # Create masked array of correct shape
-            if isinstance(value, Vector3d):
-                value = value.data
             data = crystal_map.get_map_data(value)
             data = data[self._data_slices]
 
