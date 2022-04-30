54ef7f3c7ea13315ac4d7c102912af7774b7687e
Niels Cautaerts <nielscautaerts@hotmail.com>
2022-04-30 21:16:06 +0200

    refactored a bit and added hex mesh

--- orix/sampling/S2_sampling.py
+++ orix/sampling/S2_sampling.py
@@ -21,6 +21,14 @@
 import numpy as np
 
 from orix.vector import Vector3d
+from ._polyhedral_sampling import (
+    _sample_length_equidistant,
+    _edge_grid_normalized_cube,
+    _edge_grid_spherified_edge_cube,
+    _edge_grid_spherified_corner_cube,
+    _get_max_grid_angle,
+    _compose_from_faces,
+)
 
 
 def sample_S2_uv_mesh(resolution):
@@ -66,55 +74,49 @@ def sample_S2_uv_mesh(resolution):
     return Vector3d.from_polar(azimuth=azimuth_prod, polar=polar_prod).unit
 
 
-def sample_S2_cube_mesh(resolution, grid_type="spherified_corner"):
-    """Vectors of a cube mesh on a unit sphere *S2*.
-
-    Taken from diffsims.
+def sample_S2_cube_mesh(resolution, grid_type="spherified_edge"):
+    """Vectors of a cube mesh projected on a unit sphere *S2*.
 
     Parameters
     ----------
     resolution : float
         Maximum angle between neighbour grid points, in degrees.
     grid_type : str
-        Type of cube grid: "normalized", "spherified_edge" or
-        "spherified_corner" (default).
+        Type of cube grid: "normalized", "spherified_edge" (default) or
+        "spherified_corner".
 
     Returns
     -------
     Vector3d
+
+    Notes
+    ----
+    Vectors are sampled by projecting a grid on a cube onto the unit sphere.
+    The mesh on the cube can be generated in a number of ways. A regular square
+    grid with equidistant points corresponds to the normalized option.
+    spherified_edge corresponds to points such that the row of vectors from
+    the 001 to 011 is equiangular. spherified_corner corresponds to the case
+    where the row of vectors from 001 to 111 is equiangular.
+
+    References
+    ----------
+    .. [Cajaravelli2015] O. S. Cajaravelli, "Four Ways to Create a Mesh for a Sphere,"
+        https://medium.com/game-dev-daily/four-ways-to-create-a-mesh-for-a-sphere-d7956b825db4.
     """
-    vz = Vector3d.zvector()
-    v011 = Vector3d((0, 1, 1))
-    max_angle = vz.angle_with(v011)  # = pi / 4
-    max_distance = vz.dot(v011)  # = 1
-
-    res = np.radians(resolution)
-
     grid_type = grid_type.lower()
-    grid_types = ["normalized", "spherified_edge", "spherified_corner"]
-    if grid_type == grid_types[0]:
-        grid_length = np.tan(res)
-        steps = np.ceil(max_distance / grid_length)
-        i = np.arange(-steps, steps) / steps
-    elif grid_type == grid_types[1]:
-        steps = np.ceil((max_angle / res).round(2))
-        k = np.arange(-steps, steps)
-        theta = np.arctan(max_distance) / steps
-        i = np.tan(k * theta)
-    elif grid_type == grid_types[2]:
-        v111 = Vector3d((1, 1, 1))
-        max_angle = vz.angle_with(v111)
-
-        steps = np.ceil(max_angle / res)
-        k = np.arange(-steps, steps)
-        theta = np.arctan(np.sqrt(2)) / steps
-        i = np.tan(k * theta) / np.sqrt(2)
-    else:
+    grid_mapping = {
+        "normalized": _edge_grid_normalized_cube,
+        "spherified_edge": _edge_grid_spherified_edge_cube,
+        "spherified_corner": _edge_grid_spherified_corner_cube,
+    }
+    try:
+        grid_on_edge = grid_mapping[grid_type](resolution)
+    except KeyError:
         raise ValueError(
-            f"The `grid_type` {grid_type} is not among the valid options {grid_types}"
+            f"{grid_type} is not among the valid options {list(grid_mapping.keys())}"
         )
 
-    x, y = np.meshgrid(i, i)
+    x, y = np.meshgrid(grid_on_edge, grid_on_edge)
     x = x.ravel()
     y = y.ravel()
     z = np.ones(x.shape[0])
@@ -131,3 +133,154 @@ def sample_S2_cube_mesh(resolution, grid_type="spherified_corner"):
     m_c = np.array([[-1, 1, 1], [1, -1, -1]])
 
     return Vector3d(np.vstack((bottom, top, east, west, south, north, m_c))).unit
+
+
+def sample_S2_hexagonal_mesh(resolution):
+    number_of_steps = int(np.ceil(2 / np.tan(np.radians(resolution))))
+    if number_of_steps % 2 == 1:
+        # an even number of steps is required to get a point in the middle
+        # of the hexagon edge
+        number_of_steps += 1
+    grid_1D = _sample_length_equidistant(
+        number_of_steps,
+        length=1.,
+        include_start=True,
+        include_end=True,
+        positive_and_negative=False,
+    )
+
+    # top and bottom face of the hexagon
+    axis_to_corner_1 = grid_1D[1:]
+    axis_to_corner_2 = grid_1D
+    u, v = np.meshgrid(axis_to_corner_1, axis_to_corner_2)
+    u = u.ravel()
+    v = v.ravel()
+
+    # from square to hex lattice
+    hexagon_edge_length = 2 / np.sqrt(3)
+    transform = np.array([[hexagon_edge_length, hexagon_edge_length/2], [0, 1]])
+    uv = np.stack([u, v])
+    xy = np.dot(transform, uv)
+    x, y = xy
+
+    # raise to pyramidal plane
+    z = -1/hexagon_edge_length * x - 1/2 * y + 1
+    tolerance = -1e-7
+    include_points = z > tolerance
+    points_one_face = np.stack([coordinate[include_points] for coordinate in [x, y, z]])
+
+    # repeat 6 times by rotating 60 degrees
+    def rotation(r):
+        return np.array([
+            [np.cos(r), -np.sin(r), 0],
+            [np.sin(r), np.cos(r), 0],
+            [0, 0, 1]]
+        )
+    angle = np.radians(60)
+    top_faces = np.hstack([np.dot(rotation(i*angle), points_one_face) for i in range(6)])
+    bottom_faces = top_faces.copy()
+    bottom_faces[2] *= -1
+    exclude_rim = bottom_faces[2] < tolerance
+    bottom_faces = bottom_faces.T[exclude_rim].T
+    north_pole = np.array([[0, 0, 1]]).T
+    south_pole = np.array([[0, 0, -1]]).T
+    all_points = np.hstack([top_faces, north_pole, bottom_faces, south_pole])
+    return Vector3d(all_points.T).unit
+
+
+def sample_S2_random_mesh(resolution, seed=None):
+    """Vectors of a random mesh on *S2*
+
+    Parameters
+    ----------
+    resolution : float
+        The expected mean angle between nearest neighbor
+        grid points in degrees.
+    seed : int, optional
+        passed to np.random.default_rng(), defaults to None which
+        will give a "new" random result each time
+
+    Returns
+    -------
+    Vector3d
+
+    References
+    ----------
+    https://mathworld.wolfram.com/SpherePointPicking.html
+    """
+    # convert resolution in degrees to number of points
+    number = int(1 / (4 * np.pi) * (360 / resolution) ** 2)
+    rng = np.random.default_rng(seed=seed)
+    xyz = rng.normal(size=(number, 3))
+    return Vector3d(xyz).unit
+
+
+def sample_S2_icosahedral_mesh(resolution):
+    """Vectors of an icosahedral mesh on *S2*
+
+    Parameters
+    ----------
+    resolution : float
+        Maximum angle between neighbour grid points, in degrees.
+
+    Returns
+    -------
+    Vector3d
+
+    References
+    ----------
+    .. [Meshzoo] The `meshzoo.sphere` module,
+        https://github.com/nschloe/meshzoo/blob/master/meshzoo/sphere.py.
+    """
+    t = (1.0 + np.sqrt(5.0)) / 2.0
+    corners = np.array(
+        [
+            [-1, +t, +0],
+            [+1, +t, +0],
+            [-1, -t, +0],
+            [+1, -t, +0],
+            #
+            [+0, -1, +t],
+            [+0, +1, +t],
+            [+0, -1, -t],
+            [+0, +1, -t],
+            #
+            [+t, +0, -1],
+            [+t, +0, +1],
+            [-t, +0, -1],
+            [-t, +0, +1],
+        ]
+    )
+    faces = [
+        (0, 11, 5),
+        (0, 5, 1),
+        (0, 1, 7),
+        (0, 7, 10),
+        (0, 10, 11),
+        (1, 5, 9),
+        (5, 11, 4),
+        (11, 10, 2),
+        (10, 7, 6),
+        (7, 1, 8),
+        (3, 9, 4),
+        (3, 4, 2),
+        (3, 2, 6),
+        (3, 6, 8),
+        (3, 8, 9),
+        (4, 9, 5),
+        (2, 4, 11),
+        (6, 2, 10),
+        (8, 6, 7),
+        (9, 8, 1),
+    ]
+    n = 1
+    angle = _get_max_grid_angle(corners)
+    # maybe not the most efficient approach, but the least work
+    while angle > resolution:
+        vertices = _compose_from_faces(corners, faces, n)
+        angle = _get_max_grid_angle(vertices)
+        n = n + 1
+    # push all nodes to the sphere
+    norms = np.sqrt(np.einsum("ij,ij->i", vertices, vertices))
+    vertices = (vertices.T / norms.T).T
+    return Vector3d(vertices).unit
---
+++ orix/sampling/_polyhedral_sampling.py
@@ -0,0 +1,263 @@
+import numpy as np
+from scipy.spatial import cKDTree
+
+
+def _get_start_and_end_index(
+    number_of_steps: float,
+    include_start: bool,
+    include_end: bool,
+    positive_and_negative: bool,
+):
+    if positive_and_negative:
+        start = -number_of_steps
+    else:
+        start = 0
+    if not include_start:
+        start = start + 1
+    end = number_of_steps
+    if include_end:
+        end = end + 1
+    return start, end
+
+
+def _number_of_equidistant_steps(resolution: float, length: float) -> int:
+    maximum_grid_spacing = np.tan(np.radians(resolution))
+    return int(np.ceil(length / maximum_grid_spacing))
+
+
+def _sample_length_equidistant(
+    number_of_steps: int,
+    length: float,
+    include_start: bool = True,
+    include_end: bool = False,
+    positive_and_negative: bool = True,
+):
+    start_index, end_index = _get_start_and_end_index(
+        number_of_steps,
+        include_start,
+        include_end,
+        positive_and_negative,
+    )
+    grid_spacing = length / number_of_steps
+    grid_on_edge = np.arange(start_index, end_index) * grid_spacing
+    return grid_on_edge
+
+
+def _number_of_equiangular_steps(resolution: float, length: float) -> int:
+    total_angle = np.arctan(length)
+    return int(np.ceil(total_angle / np.radians(resolution)))
+
+
+def _sample_length_equiangular(
+    number_of_steps: float,
+    length: float,
+    include_start: bool = True,
+    include_end: bool = False,
+    positive_and_negative: bool = True,
+):
+    total_angle = np.arctan(length)
+    start_index, end_index = _get_start_and_end_index(
+        number_of_steps,
+        include_start,
+        include_end,
+        positive_and_negative,
+    )
+    linear_grid = np.arange(start_index, end_index)
+    angular_increment = total_angle / number_of_steps
+    grid_on_edge = np.tan(linear_grid * angular_increment)
+    return grid_on_edge
+
+
+def _edge_grid_normalized_cube(resolution: float):
+    center_of_face_to_edge = 1.
+    number_of_steps = _number_of_equidistant_steps(resolution, center_of_face_to_edge)
+    return _sample_length_equidistant(number_of_steps, center_of_face_to_edge)
+
+
+def _edge_grid_spherified_edge_cube(resolution: float):
+    center_of_face_to_edge = 1.
+    number_of_steps = _number_of_equiangular_steps(resolution, center_of_face_to_edge)
+    return _sample_length_equiangular(number_of_steps, center_of_face_to_edge)
+
+
+def _edge_grid_spherified_corner_cube(resolution: float):
+    center_of_face_to_corner = np.sqrt(2)
+    number_of_steps = _number_of_equiangular_steps(resolution, center_of_face_to_corner)
+    grid_on_diagonal = _sample_length_equiangular(number_of_steps, center_of_face_to_corner)
+    return grid_on_diagonal / center_of_face_to_corner
+
+
+def _compose_from_faces(corners, faces, n):
+    """
+    Helper function to refine a grid starting from a platonic solid,
+    adapted from meshzoo
+
+    Parameters
+    ----------
+    corners: numpy.ndarray (N, 3)
+        Coordinates of vertices for starting shape
+    faces : list of 3-tuples of int elements
+        Each tuple in the list corresponds to the vertex indices making
+        up the face of the mesh
+    n : int
+        number of times the mesh is refined
+
+    Returns
+    -------
+    vertices: numpy.ndarray (N, 3)
+        The coordinates of the refined mesh vertices.
+
+    See also
+    --------
+    :func:`get_icosahedral_mesh_vertices`
+    """
+    # create corner nodes
+    vertices = [corners]
+    vertex_count = len(corners)
+    corner_nodes = np.arange(len(corners))
+    # create edges
+    edges = set()
+    for face in faces:
+        edges.add(tuple(sorted([face[0], face[1]])))
+        edges.add(tuple(sorted([face[1], face[2]])))
+        edges.add(tuple(sorted([face[2], face[0]])))
+    edges = list(edges)
+    # create edge nodes:
+    edge_nodes = {}
+    t = np.linspace(1 / n, 1.0, n - 1, endpoint=False)
+    corners = vertices[0]
+    k = corners.shape[0]
+    for edge in edges:
+        i0, i1 = edge
+        new_vertices = np.outer(1 - t, corners[i0]) + np.outer(t, corners[i1])
+        vertices.append(new_vertices)
+        vertex_count += len(vertices[-1])
+        edge_nodes[edge] = np.arange(k, k + len(t))
+        k += len(t)
+    triangle_cells = []
+    k = 0
+    for i in range(n):
+        j = np.arange(n - i)
+        triangle_cells.append(np.column_stack([k + j, k + j + 1, k + n - i + j + 1]))
+        j = j[:-1]
+        triangle_cells.append(
+            np.column_stack([k + j + 1, k + n - i + j + 2, k + n - i + j + 1])
+        )
+        k += n - i + 1
+    triangle_cells = np.vstack(triangle_cells)
+    for face in faces:
+        corners = face
+        edges = [(face[0], face[1]), (face[1], face[2]), (face[2], face[0])]
+        is_edge_reverted = [False, False, False]
+        for k, edge in enumerate(edges):
+            if edge[0] > edge[1]:
+                edges[k] = (edge[1], edge[0])
+                is_edge_reverted[k] = True
+        # First create the interior points in barycentric coordinates
+        if n == 1:
+            num_new_vertices = 0
+        else:
+            bary = (
+                np.hstack(
+                    [[np.full(n - i - 1, i), np.arange(1, n - i)] for i in range(1, n)]
+                )
+                / n
+            )
+            bary = np.array([1.0 - bary[0] - bary[1], bary[1], bary[0]])
+            corner_verts = np.array([vertices[0][i] for i in corners])
+            vertices_cart = np.dot(corner_verts.T, bary).T
+
+            vertices.append(vertices_cart)
+            num_new_vertices = len(vertices[-1])
+        # translation table
+        num_nodes_per_triangle = (n + 1) * (n + 2) // 2
+        tt = np.empty(num_nodes_per_triangle, dtype=int)
+        # first the corners
+        tt[0] = corner_nodes[corners[0]]
+        tt[n] = corner_nodes[corners[1]]
+        tt[num_nodes_per_triangle - 1] = corner_nodes[corners[2]]
+        # then the edges.
+        # edge 0
+        tt[1:n] = edge_nodes[edges[0]]
+        if is_edge_reverted[0]:
+            tt[1:n] = tt[1:n][::-1]
+        #
+        # edge 1
+        idx = 2 * n
+        for k in range(n - 1):
+            if is_edge_reverted[1]:
+                tt[idx] = edge_nodes[edges[1]][n - 2 - k]
+            else:
+                tt[idx] = edge_nodes[edges[1]][k]
+            idx += n - k - 1
+        #
+        # edge 2
+        idx = n + 1
+        for k in range(n - 1):
+            if is_edge_reverted[2]:
+                tt[idx] = edge_nodes[edges[2]][k]
+            else:
+                tt[idx] = edge_nodes[edges[2]][n - 2 - k]
+            idx += n - k
+        # now the remaining interior nodes
+        idx = n + 2
+        j = vertex_count
+        for k in range(n - 2):
+            for _ in range(n - k - 2):
+                tt[idx] = j
+                j += 1
+                idx += 1
+            idx += 2
+        vertex_count += num_new_vertices
+    vertices = np.concatenate(vertices)
+    return vertices
+
+
+def _get_first_nearest_neighbors(points, leaf_size=50):
+    """
+    Helper function to get an array of first nearest neighbor points
+    for all points in a point cloud
+
+    Parameters
+    ----------
+    points : numpy.ndarray (N, D)
+        Point cloud with N points in D dimensions
+    leaf_size : int
+        The NN search is performed using a cKDTree object. The way
+        this tree is constructed depends on leaf_size, so this parameter
+        will influence speed of tree construction and search.
+
+    Returns
+    -------
+    nn1_vec : numpy.ndarray (N,D)
+        Point cloud with N points in D dimensions, representing the nearest
+        neighbor point of each point in "points"
+    """
+    tree = cKDTree(points, leaf_size)
+    # get the indexes of the first nearest neighbor of each vertex
+    nn1 = tree.query(points, k=2)[1][:, 1]
+    nn1_vec = points[nn1]
+    return nn1_vec
+
+
+def _get_angles_between_nn_gridpoints(vertices, leaf_size=50):
+    """
+    Helper function to get the angles between all nearest neighbor grid
+    points on a grid of a sphere.
+    """
+    # normalize the vertex vectors
+    vertices = (vertices.T / np.linalg.norm(vertices, axis=1)).T
+    nn1_vec = _get_first_nearest_neighbors(vertices, leaf_size)
+    # the dot product between each point and its nearest neighbor
+    nn_dot = np.sum(vertices * nn1_vec, axis=1)
+    # angles
+    angles = np.rad2deg(np.arccos(nn_dot))
+    return angles
+
+
+def _get_max_grid_angle(vertices, leaf_size=50):
+    """
+    Helper function to get the maximum angle between nearest neighbor grid
+    points on a grid.
+    """
+    return np.max(_get_angles_between_nn_gridpoints(vertices, leaf_size))
