6418f535a3a658e27d77cd8df812ba6723273b36
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-05-06 12:00:17 +0200

    Set version to 0.9.0rc1 and move Paddy above Duncan in credits
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- orix/__init__.py
+++ orix/__init__.py
@@ -1,5 +1,5 @@
 __name__ = "orix"
-__version__ = "0.9.dev0"
+__version__ = "0.9.0rc1"
 __author__ = "orix developers"
 __author_email__ = "pyxem.team@gmail.com"
 __description__ = "orix is an open-source Python library for handling crystal orientation mapping data."
@@ -8,8 +8,8 @@ __credits__ = [
     "Ben Martineau",
     "Håkon Wiik Ånes",
     "Phillip Crout",
-    "Duncan Johnstone",
     "Paddy Harrison",
+    "Duncan Johnstone",
     "Niels Cautaerts",
     "Simon Høgås",
 ]
