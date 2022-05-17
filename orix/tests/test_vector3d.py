fc0a75a57b162471c1bcb5a4866d8fea836fd15a
Paddy Harrison <pj.harrison31@gmail.com>
2022-05-17 12:46:42 +0200

    remove VSCode auto-import...

--- orix/tests/test_vector3d.py
+++ orix/tests/test_vector3d.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with orix.  If not, see <http://www.gnu.org/licenses/>.
 
-from click import progressbar
 import matplotlib.pyplot as plt
 import numpy as np
 import pytest
