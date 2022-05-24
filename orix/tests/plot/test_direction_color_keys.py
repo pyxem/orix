ada17a9ac2cf7ab6a82f2d6563660d750c1f6d68
Paddy Harrison <pj.harrison31@gmail.com>
2022-05-24 12:18:57 +0200

    add tests

--- orix/tests/plot/test_direction_color_keys.py
+++ orix/tests/plot/test_direction_color_keys.py
@@ -16,6 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with orix.  If not, see <http://www.gnu.org/licenses/>.
 
+import numpy as np
+import pytest
+
 from orix.plot import IPFColorKeyTSL
 from orix.plot.direction_color_keys._util import polar_coordinates_in_sector
 from orix.quaternion import symmetry
@@ -34,3 +37,29 @@ class TestDirectionColorKeyTSL:
         sector = symmetry.C1.fundamental_sector
         rgb2 = polar_coordinates_in_sector(sector, Vector3d.xvector())
         assert rgb2[0].size == rgb2[1].size == 0
+
+    @pytest.mark.parametrize(
+        "symmetry, expected_shape, expected_xlim, expected_ylim",
+        [
+            [symmetry.C1, (2000, 2000, 3), (-1.0, 1.0), (-1.0, 1.0)],
+            [symmetry.C2, (1000, 2000, 3), (-1.0, 1.0), (0.0, 1.0)],
+            [symmetry.D6, (500, 1000, 3), (0.0, 1.0), (0.0, 0.5)],
+            [symmetry.Oh, (367, 415, 3), (0.0, 0.414), (0.0, 0.366)],
+            [symmetry.Th, (415, 415, 3), (0.0, 0.414), (0.0, 0.414)],
+        ],
+    )
+    def test_rgb_grid(self, symmetry, expected_shape, expected_xlim, expected_ylim):
+        ckey_oh = IPFColorKeyTSL(symmetry)
+        ckey_oh_direction = ckey_oh.direction_color_key
+        rgb_grid = ckey_oh_direction._create_rgb_grid()
+        assert isinstance(rgb_grid, np.ndarray)
+        assert rgb_grid.shape == expected_shape
+        (
+            _,
+            (x_min, x_max),
+            (y_min, y_max),
+        ) = ckey_oh_direction._create_rgb_grid(True)
+        assert round(x_min, 3) == round(expected_xlim[0], 3)
+        assert round(x_max, 3) == round(expected_xlim[1], 3)
+        assert round(y_min, 3) == round(expected_ylim[0], 3)
+        assert round(y_max, 3) == round(expected_ylim[1], 3)
