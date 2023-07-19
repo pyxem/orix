41861c3669b3c2e12478cdcdc1e315aed5048743
Håkon Wiik Ånes <hwaanes@gmail.com>
2023-07-19 09:06:33 -0700

    Move fixtures to conftest and increase readability of tests
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- orix/tests/quaternion/test_conversions.py
+++ orix/tests/quaternion/test_conversions.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with orix.  If not, see <http://www.gnu.org/licenses/>.
 
-import warnings
-
 import numpy as np
 import pytest
 
