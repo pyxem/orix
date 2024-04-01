63eb643259d8dae693efe8ba12baa019f308ecab
Ondrej Lexa <lexa.ondrej@gmail.com>
2024-04-01 17:50:31 +0200

    import fix

--- orix/io/plugins/__init__.py
+++ orix/io/plugins/__init__.py
@@ -32,7 +32,8 @@
     orix_hdf5
 """
 
-from orix.io.plugins import ang, bruker_h5ebsd, emsoft_h5ebsd, orix_hdf5, channel5_ctf
+from orix.io.plugins import (ang, bruker_h5ebsd, channel5_ctf, emsoft_h5ebsd,
+                             orix_hdf5)
 
 plugin_list = [
     ang,
