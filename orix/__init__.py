71f5435c71b37edc8723cbd03bd7ee47ce7633cb
Håkon Wiik Ånes <hwaanes@gmail.com>
2024-09-07 16:01:43 +0200

    Add coverage deps to CI build
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- orix/__init__.py
+++ orix/__init__.py
@@ -1,8 +1,4 @@
-__name__ = "orix"
 __version__ = "0.14.dev0"
-__author__ = "orix developers"
-__author_email__ = "pyxem.team@gmail.com"
-__description__ = "orix is an open-source Python library for handling crystal orientation mapping data."
 # Sorted by line contributions (ideally excluding lines in notebook files)
 __credits__ = [
     "Håkon Wiik Ånes",
