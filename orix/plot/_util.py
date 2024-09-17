792081852030a9d790a7db0f79623a0051dfffe1
Håkon Wiik Ånes <hwaanes@gmail.com>
2024-09-17 21:58:39 +0200

    Reintroduced Arrow3D.do_3d_projection() as it was apparently needed
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- orix/plot/_util.py
+++ orix/plot/_util.py
@@ -18,6 +18,7 @@
 from typing import Tuple, Union
 
 from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
 import numpy as np
 
 
@@ -31,6 +32,12 @@ class Arrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
         super().__init__((0, 0), (0, 0), *args, **kwargs)
         self._verts3d = xs, ys, zs
+
+    def do_3d_projection(self, renderer=None):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, self.axes.M)
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+        return np.min(zs)
 
 
 def format_labels(
