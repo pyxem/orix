8dbc43ecc161efc357ad45b51c22f53e463a7b17
Håkon Wiik Ånes <hwaanes@gmail.com>
2025-09-18 23:08:04 +0200

    Slight reformatting of Wulff net example
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- examples/stereographic_projection/wulff_net.py
+++ examples/stereographic_projection/wulff_net.py
@@ -21,50 +21,48 @@
 Wulff net
 =========
 
-Some examples for how a Wulff net can be added to any stereographic plot in
-ORIX.
+This example shows how to draw a customized Wulff net in stereographic plots using
+:meth:`~orix.plot.StereographicPlot.wulff_net`
 """
 
-import matplotlib.collections as mcollctions
 import matplotlib.pyplot as plt
-import numpy as np
 
-from orix.plot import StereographicPlot
-from orix.quaternion.symmetry import C6
-from orix.vector.vector3d import Vector3d
+from orix.plot import InversePoleFigurePlot, StereographicPlot
+from orix.quaternion.symmetry import C6h
 
-fig, ax = plt.subplots(
-    figsize=[6, 4], nrows=1, ncols=2, subplot_kw=dict(projection="stereographic")
-)
+########################################################################################
+# Plot two stereographic projections, one with the standard Wulff net, another with a
+# customized net
 
-# display a standard Wulff net with 2 degree minor markers and 10 degree
+fig = plt.figure(figsize=(6, 4), layout="tight")
+ax0 = fig.add_subplot(121, projection="stereographic")
+ax1 = fig.add_subplot(122, projection="stereographic")
+
+# Display a standard Wulff net with 2 degree minor markers and 10 degree
 # major markers, with 10 degree caps at the tops and bottoms.
-ax[0].wulff_net()
+ax0.set_title("Standard Wulff net")
+ax0.wulff_net()
 
-# The grid spacing can also be changed or turned on and off, similar to ax.grid
-# in matplotlib. Here, the latitudinal grid is set to 3 degrees (15 degrees for
-# major grid lines), and the longitudinal to 9 degrees (45 degrees for major grid
-# lines)
+# The grid spacing can also be changed or turned on and off, similar to
+# matplotlib.axes.Axes.grid(). Here, the latitudinal grid is set to 3
+# degrees (15 degrees for major grid lines), and the longitudinal to 9
+# degrees (45 degrees for major grid lines).
+ax1.set_title("Custom Wulff net")
+ax1.wulff_net(True, 3, 9, 15, 45, 15)
 
-ax[1].wulff_net(True, 3, 9, 15, 45, 15)
 # Turn it off
-ax[0].wulff_net()
-# Then turn it back on, with the previously defined grid spacing saved.
-ax[0].wulff_net()
-ax[0].set_title("Standard")
-ax[1].set_title("Custom")
-plt.tight_layout()
+ax1.wulff_net()
 
-# This also works for subsections, such as fundamental sectors.
-fig = plt.figure()
-ax = fig.add_subplot(projection="stereographic")
-ax.restrict_to_sector(C6.fundamental_sector)
+# Then turn it back on, with the previously defined grid spacing saved.
+ax1.wulff_net()
+
+########################################################################################
+# The net also displays nicely for inverse pole figures (stereographic projections
+# restricted to a fundamental sector)
+fig = plt.figure(layout="tight")
+ax = fig.add_subplot(projection="ipf", symmetry=C6h)
+
 ax.wulff_net()
-# Add some manual labels. the XY coordinates can be set by using either a single
-# vector, or a polar-azimuth pair. Both methods are showb below for a 6-fold
-# crystal.
-ax.text(Vector3d([0, 0, 1]), s="[001]", offset=[0, -0.04], c="r")
-ax.text(Vector3d([1, 0, 0]).unit, s="[100]", offset=[0, -0.04], c="g")
-ax.text(np.pi / 3, np.pi / 2, s="[010]", offset=[-0, 0], c="b")
-ax.set_title("C6 symmetry\n\n")
-plt.tight_layout()
+ax.set_title(f"{C6h.name} inverse pole figure", pad=30)
+
+plt.show()
